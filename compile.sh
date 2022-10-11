19448b5393e295640f1592d0eea0caea64b89526
Pedro Bastos <pedro.bbastos123@gmail.com>
2022-10-11 19:19:17 -0300

    Updated compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+echo "------- rm server-output ---------"
+rm -f ./server-output
 echo "------- client.c ---------"
 gcc -Wall cliente.c -o cliente
 echo "------- servidor.c -------"
 gcc -Wall servidor.c -o servidor
-
